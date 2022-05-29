b71f4e46fdb95621f3f9de890c12e776adaf3773
Aditya <adityasingh220598@gmail.com>
2022-05-29 13:45:52 +0530

    Outputting a list of meetups

--- pages/index.js
+++ pages/index.js
@@ -1,2 +1,26 @@
-const HomePage = () => {};
+import MeetupsList from "../components/meetups/MeetupList";
+
+const DUMMY_MEETUPS = [
+  {
+    id: "m1",
+    title: "First Meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg",
+    address: "someAddress",
+    description: "someDescription",
+  },
+  {
+    id: "m2",
+    title: "Second Meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg",
+    address: "someOtherAddress",
+    description: "someOtherDescription",
+  },
+];
+
+const HomePage = () => {
+  return <MeetupsList meetups={DUMMY_MEETUPS} />;
+};
+
 export default HomePage;
