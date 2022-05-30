49c2e53136b8d9a0b38bf28632652ba1ac7e8dc4
Aditya <adityasingh220598@gmail.com>
2022-05-30 08:54:35 +0530

    Sending HTTP request to the new meetup endpoint

--- pages/new-meetup.js
+++ pages/new-meetup.js
@@ -1,5 +1,8 @@
+import { useRouter } from "next/router";
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
+
 const NewMeetup = () => {
+  const router = useRouter();
   const addMeetupHandler = async (meetupData) => {
     const response = await fetch("/api/new-meetup", {
       method: "POST",
@@ -9,6 +12,9 @@ const NewMeetup = () => {
       body: JSON.stringify(meetupData),
     });
     const meetup = await response.json();
+    console.log("meetup data", meetup);
+    // Redirect user to the homepage using router
+    router.push("/");
   };
   return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
 };
