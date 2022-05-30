b885bfebb2db25b5a375ed548d4b6ffcc7cf17a8
Aditya <adityasingh220598@gmail.com>
2022-05-30 08:21:08 +0530

    Introducing the first API endpoint

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,13 +1,26 @@
-// /api/new-meetup
+import { MongoClient } from "mongodb";
 
-function handler(req, res) {
+async function handler(req, res) {
   if (req.method === "POST") {
     const data = req.body;
-    const { title, image, address, description } = data;
+    try {
+      // Connect to mongodb database using connection string
+      const client = await MongoClient.connect(
+        "mongodb+srv://root:tiktik123@cluster0.lhsfo.mongodb.net?retryWrites=true&w=majority",
+        { useNewUrlParser: true }
+      );
+      const db = await client.db("meetups");
+      const meetupsCollection = await db.collection("meetups");
+      const result = await meetupsCollection.insertOne(data);
+      const id = result.insertedId;
+      client.close();
+      res.status(201).json({ id });
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
   } else {
-    res.writeHead(405, {
-      "Content-Type": "text/plain",
+    res.status(405).json({
+      message: "Method not allowed",
     });
-    res.end("Method not allowed");
   }
 }
