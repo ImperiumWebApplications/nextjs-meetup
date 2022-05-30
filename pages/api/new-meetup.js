cb63099c47a50b9417e7423426a18b7c84e05aa0
Aditya <adityasingh220598@gmail.com>
2022-05-30 08:50:40 +0530

    Sending HTTP request to the new meetup endpoint

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -24,3 +24,5 @@ async function handler(req, res) {
     });
   }
 }
+
+export default handler;
--- pages/new-meetup.js
+++ pages/new-meetup.js
@@ -1,7 +1,14 @@
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 const NewMeetup = () => {
-  const addMeetupHandler = (meetupData) => {
-    console.log(meetupData);
+  const addMeetupHandler = async (meetupData) => {
+    const response = await fetch("/api/new-meetup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(meetupData),
+    });
+    const meetup = await response.json();
   };
   return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
 };
